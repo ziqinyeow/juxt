1939bc73a7b7bf0942d2b878553251b23599cf0d
ziqinyeow <ziqinyeow@gmail.com>
2023-09-12 12:03:47 +0800

    (Enhancement) Modified Panel Layout For Scrolling

--- components/Panel/index.tsx
+++ components/Panel/index.tsx
@@ -24,39 +24,63 @@ import Track from "./track";
 const tracks = [
   {
     name: "track 1",
-    icon: "",
-    type: "text",
-    color: "bg-element-1",
+    elements: [
+      {
+        type: "text",
+        duration: 5,
+        startAt: 2,
+      },
+    ],
   },
   {
     name: "track 2",
-    icon: "",
-    type: "video",
-    color: "bg-element-2",
+    elements: [
+      {
+        type: "video",
+        duration: 5,
+        startAt: 2,
+      },
+    ],
   },
   {
     name: "track 3",
-    icon: "",
-    type: "image",
-    color: "bg-element-3",
+    elements: [
+      {
+        type: "image",
+        duration: 5,
+        startAt: 2,
+      },
+    ],
   },
   {
     name: "track 4",
-    icon: "",
-    type: "bbox",
-    color: "bg-element-4",
+    elements: [
+      {
+        type: "pose",
+        duration: 5,
+        startAt: 2,
+      },
+    ],
   },
   {
-    name: "track 5",
-    icon: "",
-    type: "pose",
-    color: "bg-element-5",
+    name: "track 4",
+    elements: [
+      {
+        type: "pose",
+        duration: 5,
+        startAt: 2,
+      },
+    ],
   },
   {
-    name: "track 5",
-    icon: "",
-    type: "image",
-    color: "bg-element-3",
+    name: "track 4",
+    elements: [
+      {
+        type: "pose",
+        duration: 5,
+        startAt: 2,
+      },
+    ],
   },
 ];
 
@@ -117,78 +141,70 @@ const Panel = () => {
       </div>
       {!hidePanel && (
         <>
-          <div className="h-[calc(100%_-_50px)] relative min-h-[300px] overflow-y-auto panel_scrollbar max-h-[350px] flex">
-            <div className="bg-primary-500 grid grid-rows-[36px_auto] overflow-x-auto panel_scrollbar">
-              <div className="sticky top-0 z-50 flex items-end border-b bg-primary-600 border-primary-400">
-                <div className="w-[200px] sticky z-50 bg-primary-600 pb-2 border-r border-primary-400 left-0 flex items-end gap-3 px-4 h-full">
-                  <IconSearch className="w-4 h-4 stroke-[4px] text-primary-200 " />
-                  <input
-                    className="w-full tracking-widest bg-primary-600 focus:outline-none"
-                    type="text"
-                    placeholder="Search"
-                  />
-                </div>
-                <div
-                  style={{ gap: `${(120 + panelScale * 10) / panelScale}px` }}
-                  className="relative flex items-end w-full h-full px-4 pb-2 select-none"
-                >
-                  {/* <div className="absolute left-0 w-1 h-full bg-white" /> */}
-                  {Array((maxTime / 1000 + 5) * panelScale)
-                    .fill(1)
-                    .map((_, i) => (
-                      <div
-                        key={i}
-                        className="flex flex-col items-center gap-[0.5px]"
-                      >
-                        <div
-                          className={cn([
-                            i % panelScale === 0
-                              ? "text-[8px] text-primary-200"
-                              : "hidden",
-                          ])}
-                        >
-                          {i / panelScale}
-                        </div>
-                        <div
-                          className={cn([
-                            "w-[0.1px] bg-primary-200",
-                            i % panelScale == 0 ? "h-2" : "h-1",
-                          ])}
-                        ></div>
-                      </div>
-                    ))}
-                </div>
+          <div className="h-[calc(100%_-_50px)] relative grid grid-cols-[200px_auto] min-h-[300px] overflow-y-auto bg-primary-500 panel_scrollbar max-h-[350px]">
+            <div className="w-full">
+              <div className="top-0 z-50 flex items-end w-full gap-3 px-4 pb-2 border-b border-r h-9 bg-primary-600 border-primary-400">
+                <IconSearch className="w-4 h-4 stroke-[4px] text-primary-200 " />
+                <input
+                  className="w-full tracking-widest bg-primary-600 focus:outline-none"
+                  type="text"
+                  placeholder="Search"
+                />
               </div>
-              <div className="flex w-full">
-                <div className="w-[200px] sticky z-40 left-0 pt-2 border-r bg-primary-500 border-primary-400">
-                  {tracks?.map((track, i) => (
+              {tracks?.map((track, i) => (
+                <div
+                  className="relative px-2 py-4 border-b border-r border-primary-400"
+                  key={i}
+                >
+                  <div className="flex items-center justify-between h-6 gap-2 px-2 text-primary-200">
+                    <div className="flex items-center gap-2">
+                      <IconGripVertical className="w-4 h-4 text-primary-200/80 cursor-grab" />
+                      <div>Track {i + 1}</div>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <Unlock className="w-4 h-4 cursor-not-allowed text-primary-200/50" />
+                      <IconEye className="w-5 h-5 cursor-not-allowed text-primary-200/50" />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="grid grid-rows-[36px_auto] overflow-x-auto overflow-y-hidden panel_scrollbar">
+              <div
+                style={{ gap: `${(120 + panelScale * 10) / panelScale}px` }}
+                className="z-40 flex items-end w-full px-4 pb-2 border-b select-none border-primary-400 h-9"
+              >
+                {Array((maxTime / 1000 + 5) * panelScale)
+                  .fill(1)
+                  .map((_, i) => (
                     <div
-                      className="relative px-2 py-4 border-b border-primary-400"
                       key={i}
+                      className="flex flex-col items-center gap-[0.5px]"
                     >
-                      <div className="flex items-center justify-between h-6 gap-2 px-2 text-primary-200">
-                        <div className="flex items-center gap-2">
-                          <IconGripVertical className="w-4 h-4 text-primary-200/80 cursor-grab" />
-                          {/* <IconTrack className="w-5 h-5" /> */}
-                          <div>Track {i + 1}</div>
-                        </div>
-                        <div className="flex items-center gap-2">
-                          <Unlock className="w-4 h-4 cursor-not-allowed text-primary-200/50" />
-                          <IconEye className="w-5 h-5 cursor-not-allowed text-primary-200/50" />
-                        </div>
+                      <div
+                        className={cn([
+                          i % panelScale === 0
+                            ? "text-[8px] text-primary-200"
+                            : "hidden",
+                        ])}
+                      >
+                        {i / panelScale}
                       </div>
+                      <div
+                        className={cn([
+                          "w-[0.1px] bg-primary-200",
+                          i % panelScale == 0 ? "h-2" : "h-1",
+                        ])}
+                      ></div>
                     </div>
                   ))}
-                </div>
-                <div className="w-full px-4 pt-2">
-                  {tracks?.map((track, i) => (
-                    <div className="relative py-4" key={i}>
-                      <div className="flex h-6 ">
-                        <Track track={track} />
-                      </div>
-                    </div>
-                  ))}
-                </div>
+              </div>
+              <div className="w-full">
+                {tracks?.map((track, i) => (
+                  <div key={i} className="w-full p-4">
+                    <Track className="relative w-full h-6" track={track} />
+                  </div>
+                ))}
               </div>
             </div>
           </div>
--- components/Panel/track.tsx
+++ components/Panel/track.tsx
@@ -4,56 +4,69 @@ import { cn } from "@/lib/utils";
 import React, { useState } from "react";
 import { Rnd } from "react-rnd";
 import { getElementColor, getElementIcon } from "./utils";
+import { DivProps } from "@/lib/types/html";
 
-const Track = ({ track }: any) => {
+export const TrackElement = ({ element }: any) => {
   const [state, setState] = useState({
     width: 200,
     height: 24,
-    x: 100,
+    x: 0,
     y: 0,
   });
 
   return (
-    <div className="relative w-full h-6">
-      <Rnd
-        className={cn([
-          "rounded select-none ring-2 ring-primary-200 ring-offset-primary-500 ring-offset-[3px]",
-          getElementColor(track.type),
-        ])}
-        bounds="parent"
-        enableResizing={{
-          top: false,
-          right: true,
-          bottom: false,
-          left: true,
-          topRight: false,
-          bottomRight: false,
-          bottomLeft: false,
-          topLeft: false,
-        }}
-        size={{ width: state.width, height: state.height }}
-        position={{ x: state.x, y: state.y }}
-        onDragStop={(e, d) => {
-          setState((p) => {
-            return { ...p, x: d.x, y: d.y };
-          });
-        }}
-        onResizeStop={(e, direction, ref, delta, position) => {
-          setState({
-            width: Number(ref.style.width.replace("px", "")),
-            height: Number(ref.style.height.replace("px", "")),
-            ...position,
-          });
-        }}
-      >
-        <div className="absolute top-0 left-0 z-10 w-full h-full bg-repeat-space bg-contain bg-voice" />
-        <div className="flex items-center w-full h-full gap-2 px-2 font-bold text-black">
-          <div className="z-20 select-none line-clamp-1 [&>*]:w-4 [&>*]:h-4">
-            {getElementIcon(track.type)}
-          </div>
-          <div className="z-20 select-none line-clamp-1">{track.type}</div>
+    <Rnd
+      className={cn([
+        "rounded select-none ring-2 ring-primary-200 ring-offset-primary-500 ring-offset-[3px]",
+        getElementColor(element.type),
+      ])}
+      bounds="parent"
+      enableResizing={{
+        top: false,
+        right: true,
+        bottom: false,
+        left: true,
+        topRight: false,
+        bottomRight: false,
+        bottomLeft: false,
+        topLeft: false,
+      }}
+      size={{ width: state.width, height: state.height }}
+      position={{ x: state.x, y: state.y }}
+      onDragStop={(e, d) => {
+        setState((p) => {
+          return { ...p, x: d.x, y: d.y };
+        });
+      }}
+      onResizeStop={(e, direction, ref, delta, position) => {
+        setState({
+          width: Number(ref.style.width.replace("px", "")),
+          height: Number(ref.style.height.replace("px", "")),
+          ...position,
+        });
+      }}
+    >
+      <div className="absolute top-0 left-0 z-10 w-full h-full bg-repeat-space bg-contain bg-voice" />
+      <div className="flex items-center w-full h-full gap-2 px-2 font-bold text-black">
+        <div className="z-20 select-none line-clamp-1 [&>*]:w-4 [&>*]:h-4">
+          {getElementIcon(element.type)}
         </div>
-      </Rnd>
+        <div className="z-20 select-none line-clamp-1">{element.type}</div>
+      </div>
+    </Rnd>
+  );
+};
+
+type TrackProps = DivProps & {
+  track: any;
+};
+
+export const Track = ({ track, ...props }: TrackProps) => {
+  return (
+    <div {...props}>
+      {track.elements.map((element: any, i: number) => (
+        <TrackElement key={i} element={element} />
+      ))}
     </div>
   );
 };
