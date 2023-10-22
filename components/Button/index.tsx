fdec5a5552c92cf1693ea1360544a25c30d544d5
ziqinyeow <ziqinyeow@gmail.com>
2023-10-22 15:44:00 +0800

    (Fix) Fix Keyboard Shortcut When Button Focus + Blur

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,19 +1,29 @@
 import { cn } from "@/lib/utils";
 import { type ButtonProps } from "@/lib/types/html";
+import { useStore } from "@/lib/store";
 
-export const Button = ({ className, children, ...props }: ButtonProps) => (
-  <button
-    {...props}
-    className={cn([
-      "p-1 hover:bg-light-400 dark:hover:bg-primary-500 hover:text-opacity-75 rounded",
-      "disabled:bg-opacity-0 disabled:text-white/30 disabled:hover:text-white/30",
-      "transition-all",
-      className,
-    ])}
-  >
-    {children}
-  </button>
-);
+export const Button = ({ className, children, ...props }: ButtonProps) => {
+  const { setDisableKeyboardShortcut } = useStore();
+  return (
+    <button
+      {...props}
+      onFocus={() => {
+        setDisableKeyboardShortcut(true);
+      }}
+      onBlur={() => {
+        setDisableKeyboardShortcut(false);
+      }}
+      className={cn([
+        "p-1 hover:bg-light-400 dark:hover:bg-primary-500 hover:text-opacity-75 rounded",
+        "disabled:bg-opacity-0 disabled:text-white/30 disabled:hover:text-white/30",
+        "transition-all",
+        className,
+      ])}
+    >
+      {children}
+    </button>
+  );
+};
 
 export const ToolbarButton = ({
   className,
