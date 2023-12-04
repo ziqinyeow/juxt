80e06eead5391944b7ea345920455a63fda51f4e
ziqinyeow <ziqinyeow@gmail.com>
2023-12-04 21:31:33 +0800

    (Canvas Context Menu) Completed Clone Of Track Style + Right Click Pop Up Context Window On Canvas

--- apps/web/components/Canvas/index.tsx
+++ apps/web/components/Canvas/index.tsx
@@ -9,7 +9,11 @@ import React, { useEffect, useState } from "react";
 import { useStore } from "@/lib/store";
 import Tooltip from "@/components/Tooltip";
 import { Button } from "@/components/Button";
-import { IconFocusCentered } from "@tabler/icons-react";
+import {
+  IconFocusCentered,
+  IconLayoutGridAdd,
+  IconYoga,
+} from "@tabler/icons-react";
 import { Minus, Plus } from "lucide-react";
 import {
   DropdownMenu,
@@ -54,7 +58,7 @@ const Canvas = () => {
   //   console.log(projects);
 
   return (
-    <div className="w-full h-full relative">
+    <div className="w-full h-full">
       <div className="absolute top-0 flex items-center h-full justify-center gap-2 left-1">
         <div className="flex gap-2 flex-col">
           <Tools tools={tools} />
@@ -151,89 +155,199 @@ const Canvas = () => {
           </Button>
         </Tooltip>
       </div>
-      <_Canvas
-        init={(c) => {
-          setCanvas(c.canvas);
-          setHandler(c.handler);
-          // refreshTracks();
-        }}
-        // ref={(c: any) => {
-        //   //   setCanvas(c?.canvas);
-        // }}
-        minZoom={20}
-        maxZoom={500}
-        objectOption={{
-          stroke: "rgba(255, 255, 255, 0)",
-          strokeUniform: true,
-          resource: {},
-          link: {
-            enabled: false,
-            type: "resource",
-            state: "new",
-            dashboard: {},
-          },
-          tooltip: {
-            enabled: true,
-            type: "resource",
-            template: "<div>{{message.name}}</div>",
-          },
-          animation: {
-            type: "none",
-            loop: true,
-            autoplay: true,
-            duration: 1000,
-          },
-          userProperty: {},
-          trigger: {
-            enabled: false,
-            type: "alarm",
-            script: "return message.value > 0;",
-            effect: "style",
-          },
-        }}
-        propertiesToInclude={[
-          "id",
-          "name",
-          "locked",
-          "file",
-          "src",
-          "link",
-          "tooltip",
-          "animation",
-          "layout",
-          "workareaWidth",
-          "workareaHeight",
-          "videoLoadType",
-          "autoplay",
-          "shadow",
-          "muted",
-          "loop",
-          "code",
-          "icon",
-          "userProperty",
-          "trigger",
-          "configuration",
-          "superType",
-          "points",
-          "svg",
-          "loadType",
-        ]}
-        className="w-full h-full top-0 left-0"
-        canvasOption={{
-          // uniformScaling: false,
-          backgroundColor: theme === "dark" ? "#000" : "#F1F0F0",
-          selectionColor:
-            theme === "dark"
-              ? "rgb(43, 235, 200, 0.15)"
-              : "rgb(93, 70, 243, 0.15)",
-          selectionBorderColor:
-            theme === "dark" ? "rgb(43, 235, 200)" : "rgb(93, 70, 243)",
-          selectionLineWidth: 2,
-        }}
-        onZoom={(zoomRatio) => {
-          setCanvasZoomRatio(zoomRatio);
-        }}
-      />
+      <div className="w-full h-full relative">
+        <_Canvas
+          className="w-full h-full absolute top-0 left-0"
+          init={(c) => {
+            setCanvas(c.canvas);
+            setHandler(c.handler);
+            // refreshTracks();
+          }}
+          // ref={(c: any) => {
+          //   //   setCanvas(c?.canvas);
+          // }}
+          onContext={(ref, event, target) => {
+            // if ((target && target.id === "workarea") || !target) {
+            //   const { layerX: left, layerY: top } = event;
+            //   return (
+            //     <Menu>
+            //       <Menu.SubMenu
+            //         key="add"
+            //         style={{ width: 120 }}
+            //         title={i18n.t("action.add")}
+            //       >
+            //         {this.transformList().map((item) => {
+            //           const option = Object.assign({}, item.option, {
+            //             left,
+            //             top,
+            //           });
+            //           const newItem = Object.assign({}, item, { option });
+            //           return (
+            //             <Menu.Item style={{ padding: 0 }} key={item.name}>
+            //               {this.itemsRef.renderItem(newItem, false)}
+            //             </Menu.Item>
+            //           );
+            //         })}
+            //       </Menu.SubMenu>
+            //     </Menu>
+            //   );
+            // }
+            // if (target?.type === "activeSelection") {
+            //   return <div>asdfopasd</div>;
+            // }
+            // if (target?.type === "group") {
+            //   return (
+            //     <div>group</div>
+            //     // <Menu>
+            //     //   <Menu.Item
+            //     //     onClick={() => {
+            //     //       this.canvasRef.handler.toActiveSelection();
+            //     //     }}
+            //     //   >
+            //     //     {i18n.t("action.object-ungroup")}
+            //     //   </Menu.Item>
+            //     //   <Menu.Item
+            //     //     onClick={() => {
+            //     //       this.canvasRef.handler.duplicate();
+            //     //     }}
+            //     //   >
+            //     //     {i18n.t("action.clone")}
+            //     //   </Menu.Item>
+            //     //   <Menu.Item
+            //     //     onClick={() => {
+            //     //       this.canvasRef.handler.remove();
+            //     //     }}
+            //     //   >
+            //     //     {i18n.t("action.delete")}
+            //     //   </Menu.Item>
+            //     // </Menu>
+            //   );
+            // }
+            return (
+              <div>
+                <div
+                  className={cn(
+                    "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+                    "flex items-center gap-2 cursor-pointer hover:bg-secondary-200 focus:text-black"
+                  )}
+                >
+                  <div className="">
+                    <IconYoga className="w-4 h-4" />
+                  </div>
+                  <div className="text-[12px] font-bold tracking-widest">
+                    {/* @ts-ignore */}
+                    Run pose estimation
+                    {/* {element.properties?.pose?.length > 0
+                    ? "Rerun pose estimation"
+                    : "Run pose estimation"} */}
+                  </div>
+                </div>
+                <div
+                  className={cn(
+                    "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+                    "flex items-center gap-2 cursor-pointer hover:bg-secondary-200 focus:text-black"
+                  )}
+                >
+                  <div className="">
+                    <IconLayoutGridAdd className="w-4 h-4" />
+                  </div>
+                  <div className="text-[12px] font-bold tracking-widest">
+                    View Chart
+                  </div>
+                </div>
+              </div>
+              // <Menu>
+              //   <Menu.Item
+              //     onClick={() => {
+              //       this.canvasRef.handler.duplicateById(target.id);
+              //     }}
+              //   >
+              //     {i18n.t("action.clone")}
+              //   </Menu.Item>
+              //   <Menu.Item
+              //     onClick={() => {
+              //       this.canvasRef.handler.removeById(target.id);
+              //     }}
+              //   >
+              //     {i18n.t("action.delete")}
+              //   </Menu.Item>
+              // </Menu>
+            );
+          }}
+          minZoom={20}
+          maxZoom={500}
+          objectOption={{
+            stroke: "rgba(255, 255, 255, 0)",
+            strokeUniform: true,
+            resource: {},
+            link: {
+              enabled: false,
+              type: "resource",
+              state: "new",
+              dashboard: {},
+            },
+            tooltip: {
+              enabled: true,
+              type: "resource",
+              template: "<div>{{message.name}}</div>",
+            },
+            animation: {
+              type: "none",
+              loop: true,
+              autoplay: true,
+              duration: 1000,
+            },
+            userProperty: {},
+            trigger: {
+              enabled: false,
+              type: "alarm",
+              script: "return message.value > 0;",
+              effect: "style",
+            },
+          }}
+          propertiesToInclude={[
+            "id",
+            "name",
+            "locked",
+            "file",
+            "src",
+            "link",
+            "tooltip",
+            "animation",
+            "layout",
+            "workareaWidth",
+            "workareaHeight",
+            "videoLoadType",
+            "autoplay",
+            "shadow",
+            "muted",
+            "loop",
+            "code",
+            "icon",
+            "userProperty",
+            "trigger",
+            "configuration",
+            "superType",
+            "points",
+            "svg",
+            "loadType",
+          ]}
+          canvasOption={{
+            // uniformScaling: false,
+            backgroundColor: theme === "dark" ? "#000" : "#F1F0F0",
+            selectionColor:
+              theme === "dark"
+                ? "rgb(43, 235, 200, 0.15)"
+                : "rgb(93, 70, 243, 0.15)",
+            selectionBorderColor:
+              theme === "dark" ? "rgb(43, 235, 200)" : "rgb(93, 70, 243)",
+            selectionLineWidth: 2,
+          }}
+          onZoom={(zoomRatio) => {
+            setCanvasZoomRatio(zoomRatio);
+          }}
+        />
+      </div>
     </div>
   );
 };
